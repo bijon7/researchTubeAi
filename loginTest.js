c7aa8a4d1b883ae2c85766e9875fbfe0e34d44d2
Zubaer <bijon7@yahoo.com>
2024-06-17 23:47:15 -0700

    Add intial code

---
+++ loginTest.js
@@ -0,0 +1,19 @@
+
+async function automateLogin() {
+  const driver = new Builder().forBrowser('chrome').build();
+
+  try {
+    await driver.get('http://researchtube.ai/login');
+
+    await driver.findElement(By.Name('Email')).sendKeys('zubaer@researchtube.ai');
+    await driver.findElement(By.Name('Password')).sendKeys('1234', Key.Login);
+    //Looking up id "Home" after log in.
+    await driver.wait(until.elementLocated(By.id('Home')), 10000);
+
+    console.log('Login successful!');
+  } finally {
+    await driver.quit();
+  }
+}
+
+automateLogin();
